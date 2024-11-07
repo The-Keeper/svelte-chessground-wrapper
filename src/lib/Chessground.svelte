87bac8284a8f007a7ec40900855d524e0bff9e42
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-08 01:05:31 +0500

    chessground api

---
+++ src/lib/Chessground.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { Chessground } from "chessground";
+    import type { Config as CGConfig } from "chessground/config";
+    import type { Api } from "chessground/api";
+    import { onMount } from "svelte";
+
+
+
+    let { config = $bindable(), api, ...props }: { config: CGConfig, api: Api } = $props();
+
+    let container: HTMLElement;
+
+    onMount(() => {
+        api = Chessground(container, config);
+    })
+</script>
+
+<div bind:this={container}>
+    
+</div>
