efd9071d2d29587cb67b90fb084292df6cca3fc7
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-08 02:43:00 +0500

    config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
     import Chessground from "$lib/Chessground.svelte";
     import type { Api } from "chessground/api";
-    import type { Config } from "chessground/config";
     import { onMount } from "svelte";
 
     let api: Api = $state(null as unknown as Api);
-    let config: Config = $state({});
+    let config = $state({})
 
 onMount(() => {
 })
@@ -15,12 +14,12 @@ $inspect(api);
 </script>
 
 <button onclick={ () => { 
-            config = { fen: 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1' }
+    config = { fen: 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1' }
 } }> setting </button>
 <button onclick={ () => {
     api.move("e7", "e5")
 } }> api </button>
 
 <div style="width: 500px;">
-    <Chessground {config} {api}></Chessground>
+    <Chessground {config} bind:api={api}></Chessground>
 </div>
