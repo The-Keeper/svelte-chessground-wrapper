afb40be6e774da6145009a538b3cb059b38191ad
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-08 01:38:32 +0500

    chessground

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Chessground from "$lib/Chessground.svelte";
+    import type { Api } from "chessground/api";
+    import type { Config } from "chessground/config";
+    import { onMount } from "svelte";
+
+    let api: Api = $state(null as unknown as Api);
+    let config: Config = $state({});
+
+onMount(() => {
+    $inspect(api)
+    $inspect(config)
+})
+
+</script>
+
+<button onclick={ () => { 
+            config = { fen: 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1' }
+} }> setting </button>
+<button onclick={ () => {
+    api.set({fen: `rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2`})
+} }> api </button>
+
+<Chessground {config} {api}></Chessground>
